996edc7c2774db2ce2b2361d5e2f009296820d26
Zsupányi Krisztián <zsupi1987@gmail.com>
2025-02-12 14:58:08 +0100

    - Added FDCAN Bus off interrupt

--- os/hal/ports/STM32/LLD/FDCANv2/hal_can_lld.c
+++ os/hal/ports/STM32/LLD/FDCANv2/hal_can_lld.c
@@ -520,7 +520,7 @@ bool can_lld_start(CANDriver *canp) {
   canp->fdcan->IR     = (uint32_t)-1;
   canp->fdcan->IE     = FDCAN_IE_RF1NE | FDCAN_IE_RF1LE |
                         FDCAN_IE_RF0NE | FDCAN_IE_RF0LE |
-                        FDCAN_IE_TCE;
+                        FDCAN_IE_TCE | FDCAN_IE_BOE;
   canp->fdcan->TXBTIE = FDCAN_TXBTIE_TIE;
   canp->fdcan->ILE    = FDCAN_ILE_EINT0;
 
@@ -780,6 +780,11 @@ void can_lld_serve_interrupt(CANDriver *canp) {
   /* Overflow events.*/
   if (((ir & FDCAN_IR_RF0L) != 0U) || ((ir & FDCAN_IR_RF1L) != 0U)) {
     _can_error_isr(canp, CAN_OVERFLOW_ERROR);
+  }
+
+  /* Bus_off events.*/
+  if ((ir & FDCAN_IR_BO) != 0U)  {
+   _can_error_isr(canp, CAN_BUS_OFF_ERROR);
   }
 
   /* TX events.*/
